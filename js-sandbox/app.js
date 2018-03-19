dc66beffeb8d13c7f9d3d0820690e7d95af612fa
kelsS <kswanson426@gmail.com>
2018-03-19 15:09:16 -0400

    add data type nots

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -2,3 +2,31 @@
 *** JS SANDBOX: SECTION 2 JS
 */
 
+/*
+*** JS Types
+*/
+
+// PRIMITIVE
+
+// String
+const name = "John Doe";
+// Number
+const age = 24;
+// Null
+const car = null;
+// Undefined
+let test;
+// ES6 Feature
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ["movies", "music"];
+// Object Literal
+const address = {
+    city: "Boston",
+    state: "MA"
+}
+// Date Constructor
+const today = new Date();
